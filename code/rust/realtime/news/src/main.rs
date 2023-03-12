0ed35b6439139a2a9c7075e79ca67049e75ebf68
cmasfo-github <cmasfo@gmail.com>
2023-03-12 19:12:44 +0900

    save

--- code/rust/realtime/news/src/main.rs
+++ code/rust/realtime/news/src/main.rs
@@ -1,23 +1,31 @@
 use reqwest::blocking::get;
 use select::document::Document;
-use select::predicate::{Name, Predicate};
+use select::predicate::{Class, Name};
+use select::predicate::Predicate;
 use rand::prelude::SliceRandom;
 
 fn main() {
   loop {
-    // Fetch the Reuters homepage and extract the news headlines
-    let response = get("https://www.reuters.com/").unwrap();
+    // Fetch the NPR homepage and extract the news headlines and links
+    let response = get("https://www.npr.org/").unwrap();
     let document = Document::from_read(response).unwrap();
     let headlines: Vec<_> = document
-    .find(Name("h3").and(Name("a")))
-    .map(|headline| headline.text())
+    .find(Name("h3").and(Class("title")))
+    .map(|headline| {
+      let link = headline
+      .parent()
+      // .and_then(|parent| parent.find(Name("a")).next())
+      .and_then(|link| link.attr("href"));
+      (headline.text(), link.unwrap_or_default())
+    })
     .collect();
     
-    // Choose a random headline and print it
-    if let Some(headline) = headlines.choose(&mut rand::thread_rng()) {
-      println!("{}", headline);
+    // Choose a random headline and print it with its link
+    if let Some((headline, link)) = headlines.choose(&mut rand::thread_rng()) {
+      if *link == "" { continue; };
+      println!("\n{}\n > {}", headline, link);
     } else {
-      println!("Err loading headline.");
+      println!("Error loading headline.");
     }
     
     // Wait 5 seconds before fetching another headline
